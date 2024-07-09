f73a179aba9ff3c3df24701f5db867f376cab7d7
Anthony David <anthony.david34@sfr.fr>
2024-07-09 11:39:36 +0200

    initial / setup db

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+const AuthentificationLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="h-screen flex items-center justify-center">{children}</div>
+  );
+};
+
+export default AuthentificationLayout;
